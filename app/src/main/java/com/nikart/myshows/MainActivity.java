e877ca5541e6dc77aa22892e0531ed2f4fd107d6
artem-zaitsev <zaitsev_art@inbox.ru>
2016-11-17 16:53:18 +0300

    MainActivity: create a drawer, create new layouts. Rename fragment_my_shows to fragment_no_content.

--- app/src/main/java/com/nikart/myshows/MainActivity.java
+++ app/src/main/java/com/nikart/myshows/MainActivity.java
@@ -2,20 +2,33 @@ package com.nikart.myshows;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+/*
+*  Реализуем NavigationDrawer
+*  Нужны дополнительные layouts, в них вынесем туллбар,контент, drawer
+ */
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     final private static int NUM_PAGES = 3;
 
     private ViewPager mainActivityPager;
     private PagerAdapter mainActivityPagerAdapter;
+    private DrawerLayout drawer;
+    private Toolbar myToolbar;
 
     static void start(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
@@ -27,16 +40,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        myToolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(myToolbar);
 
         mainActivityPager = (ViewPager) findViewById(R.id.activity_main_viewpager);
         mainActivityPagerAdapter = new MainActivityFragmentPagerAdapter(getSupportFragmentManager());
         mainActivityPager.setAdapter(mainActivityPagerAdapter);
+
+        /* Делаем drawer
+        *  и сеттим на навигэйшнВью слушатель( пока нафиг не нужен, тк
+        *  нет контента).
+        *  Дровер не совсем нормально работает - серая полоска напрягает.
+        *  Видимо это полоска статус бара.
+        *  */
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, myToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /* Тут реализуем метод интерфейса. Особо не реализован))
+    * */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+
+    /*
+    * Ну тут короче все ясно.
+    * */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private class MainActivityFragmentPagerAdapter extends FragmentPagerAdapter {
-        public MainActivityFragmentPagerAdapter(FragmentManager supportFragmentManager) {
+        MainActivityFragmentPagerAdapter(FragmentManager supportFragmentManager) {
             super(supportFragmentManager);
         }
 
--- app/src/main/java/com/nikart/myshows/MyShowsFragment.java
+++ app/src/main/java/com/nikart/myshows/MyShowsFragment.java
@@ -1,6 +1,5 @@
 package com.nikart.myshows;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,6 +14,6 @@ public class MyShowsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_shows,container,false);
+        return inflater.inflate(R.layout.fragment_no_content,container,false);
     }
 }
