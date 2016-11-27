388669da26c887f6b4e4bcbf14fe425cc8ae1641
artem-zaitsev <zaitsev_art@inbox.ru>
2016-11-27 23:25:32 +0300

    Remove NavigationDrawer. MainActivity: create openFragment method.

--- app/src/main/java/com/nikart/main/MainActivity.java
+++ app/src/main/java/com/nikart/main/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Fragment showsFragment;
     private Fragment episodesFragment;
     private Fragment accountFragment;
-    private FragmentTransaction transaction;
+
 
     public static void start(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
@@ -44,60 +44,42 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
 
-
-        /* Делаем drawer
-        *  и сеттим на навигэйшнВью слушатель( пока нафиг не нужен, тк
-        *  нет контента).
-        * */
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
         showsFragment = new MyShowsFragment();
         episodesFragment = new MyEpisodesFragment();
         accountFragment = new AccountFragment();
-        /*transaction = getSupportFragmentManager()
-                .beginTransaction();
-        transaction.add(R.id.main_container, showsFragment);
-        transaction.commit();*/
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.main_container, showsFragment).commit();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.main_activity_bottom_nav);
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case (R.id.menu_item_my_shows):{
-                        transaction = getSupportFragmentManager().beginTransaction();
-                        transaction.replace(R.id.main_container,
-                                showsFragment);
-                        transaction.commit();
+                    case (R.id.menu_item_my_shows): {
+                        openFragment(showsFragment);
                         return true;
                     }
                     case (R.id.menu_item_my_episodes): {
-                        transaction = getSupportFragmentManager().beginTransaction();
-                        transaction.replace(R.id.main_container,
-                                episodesFragment);
-                        transaction.commit();
+                        openFragment(episodesFragment);
                         return true;
                     }
-                    case (R.id.menu_item_account):{
-                        transaction = getSupportFragmentManager().beginTransaction();
-                        transaction.replace(R.id.main_container,
-                                accountFragment);
-                        transaction.commit();
+                    case (R.id.menu_item_account): {
+                        openFragment(accountFragment);
                         return true;
                     }
-                    default: return false;
+                    default:
+                        return false;
                 }
 
             }
         });
+    }
+
+    private void openFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,
+                fragment).commit();
     }
 
     /* Тут реализуем метод интерфейса. Особо не реализован))
@@ -112,11 +94,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     * */
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        super.onBackPressed();
     }
 }
